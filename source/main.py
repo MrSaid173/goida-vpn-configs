b2be92aa2e5a3a7b9f007c4c612b2d96cda5b2f6
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-01-22 02:32:44 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -17,10 +17,10 @@ MMDB_PATH = os.path.join(BASE_DIR, "GeoLite2-Country.mmdb")
 
 EXCLUDE_PROTOCOLS = ("ss://", "trojan://", "vmess://")
 EXCLUDE_KEYWORDS = ("type=ws") #("openproxy", "type=ws")
-MAX_CONFIGS = 100 
-MAX_PER_SUBNET = 2 
-MAX_PER_SNI = 5
-MAX_PER_ID = 2
+MAX_CONFIGS = 1000 
+MAX_PER_SUBNET = 3 
+MAX_PER_SNI = 3
+MAX_PER_ID = 3
 MAX_RU_CONFIGS = 5
 
 # --- ИНИЦИАЛИЗАЦИЯ ---
