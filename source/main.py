a7fbd6bbfc8564b7cb7b952cc9cd5dc203e0eea2
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-02-11 22:37:22 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -73,6 +73,7 @@ COUNTRY_MAP = {
     "HU": {"aliases": ["HUNGARY", "ВЕНГРИЯ", "🇭🇺"], "full": "Hungary", "flag": "🇭🇺"},
     "ES": {"aliases": ["SPAIN", "ИСПАНИЯ", "🇪🇸"], "full": "Spain", "flag": "🇪🇸"},
     "IR": {"aliases": ["IRAN", "ИРАН", "🇮🇷"], "full": "Iran", "flag": "🇮🇷"},
+    "KR": {"aliases": ["ROK", "KOREA", "ЮЖНАЯ КОРЕЯ", "🇰🇷"], "full": "S.Korea", "flag": "🇰🇷"},
 }
 
 lock = threading.Lock()
