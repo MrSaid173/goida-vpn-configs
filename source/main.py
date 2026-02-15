97f998d207d306af9296bdd735ec44bc0b09d066
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-02-16 01:52:28 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -249,7 +249,7 @@ def get_exposed_tag(host):
     if w: return "W"
     return None
 
-def rename_config(link, country_code, index, is_hosting=False, is_white_sni=False):
+def rename_config(link, country_code, index, is_hosting=False, is_white_sni=False, exp_tag=None):
     country_info = COUNTRY_MAP.get(country_code, {"full": country_code, "flag": "🌐"})
     tags = []
     if is_hosting is True: tags.append("HOST")
