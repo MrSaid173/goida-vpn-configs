2cd244b54c40ca82ba5596c17d9cb88ee55ba08f
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-02-20 12:43:28 +0300

    Update find_ru_proxy.py

--- source/find_ru_proxy.py
+++ source/find_ru_proxy.py
@@ -26,8 +26,8 @@ MAX_PROXY_CONFIGS  = 5              # сколько рабочих конфиг
 BAD_HOSTING_KEYWORDS = [
     "cloudflare", "hetzner", "digitalocean", "vultr", "amazon", "google",
     "microsoft", "ovh", "linode", "servers", "work", "oracle", "leaseweb",
-    "m247", "akamai", "host" #"baykov", "dataforest", "yandex", "selectel",
-    #"timeweb", "beget"
+    "m247", "akamai", "host", "baykov", "dataforest", "yandex", "selectel",
+    "timeweb", "beget"
 ]
 EXCLUDED_SNI = ["userapi", "splitter.wb.ru"]
 
@@ -88,8 +88,6 @@ def check_isp_ru_nonhosting(ip):
             return False
         full = f"{r.get('isp')} {r.get('org')} {r.get('as')} {r.get('asname')}".lower()
         if any(w in full for w in BAD_HOSTING_KEYWORDS):
-            return False
-        if r.get("hosting", False):
             return False
         return True
     except:
