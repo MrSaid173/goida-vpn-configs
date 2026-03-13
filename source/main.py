ce9b602e139436cb69d3123f0d67e8bc15d7f58b
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-03-14 00:49:41 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -737,11 +737,11 @@ def try_add_to_lists(entry: dict) -> bool:
     is_hosting = entry['is_hosting']
 
     # RU конфиги — только SNI-RU
-    if is_ru and not is_white and not is_ss and not is_hy2:
+    if is_ru and not is_white:
         return False
     # RU конфиги не должны быть хостинговыми
-    if is_ru and is_hosting is True:
-        return False
+    #if is_ru and is_hosting is True:
+        #return False
     
     added_vlm = False
     added_vlm2 = False
