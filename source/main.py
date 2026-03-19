ee5e95be0e58791f82c7341aac0dfaf545bd7d1f
MrSaid173 <saidmurtazaliev000@gmail.com>
2026-03-19 21:50:29 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1132,8 +1132,6 @@ def validate(config: str, is_priority: bool, is_white: bool) -> None:
             if sni_vlm_count >= MAX_TOTAL_SNI_RU and sni_vlm2_count >= MAX_TOTAL_SNI_RU:
                 _inc_stat('sni_ru_limit_early')
                 return
-            _inc_stat('id_limit')
-            return
 
     # Первый пинг
     p1 = fast_ping(host, port, sni)
